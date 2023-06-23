82a65b220b269bb8858ca4136c36ccc4d4d323a2
6ivkin <fedorterentev48@gmail.com>
2023-06-23 12:41:39 +0400

    github search users

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 import {Search} from "./modules/search.js";
 import {View} from "./modules/view.js";
+import {Api} from "./modules/api.js";
+import {Log} from "./modules/log.js";
 
+const api = new Api();
+const log = new Log();
 
 const view = new View();
-const search = new Search(view);
+const search = new Search(view, api, log);
--- modules/search.js
+++ modules/search.js
@@ -1,54 +1,77 @@
-const USER_PER_PAGE = 20;
-
 export class Search {
-  setCurrentPage(pageNumber) {
-    this.currentPage = pageNumber;
-  }
-
-  constructor(view) {
-    this.view = view;
-
-    this.view.searchInput.addEventListener('keyup', this.debounce(this.loadUsers.bind(this), 500));
-    this.view.loadMore.addEventListener('click', this.loadUsers.bind(this));
-    this.currentPage = 1;
-    this.usersCount = 0;
-  }
-
-  async loadUsers() {
-    const searchValue = this.view.searchInput.value;
-    let totalCount;
-    let users;
-    try {
-      if (searchValue) {
-        const response = await fetch(`https://api.github.com/search/users?q=${searchValue}&per_page=${USER_PER_PAGE}&page=${this.currentPage}`);
-        if (response.ok) {
-          this.currentPage++;
-          const data = await response.json();
-          users = data.items;
-          totalCount = data.total_count;
-          this.setUsersCount(this.usersCount + data.items.length);
-          this.view.toggleLoadMoreBtn(totalCount > USER_PER_PAGE && this.usersCount !== totalCount);
-          users.forEach(user => this.view.createUser(user));
-        }
-      } else {
-        this.clearUsers();
-      }
-    } catch (error) {
-      console.error(error);
+    setCurrentPage(pageNumber) {
+        this.currentPage = pageNumber;
     }
-  }
 
-  clearUsers() {
-    this.view.usersList.innerHTML = '';
-  }
+    setUsersCount(count) {
+        this.usersCount = count;
+    }
 
-  debounce(func, delay) {
-    let timeoutId;
-    return function (...args) {
-      clearTimeout(timeoutId);
-      timeoutId = setTimeout(() => {
-        func.apply(this, args);
-      }, delay);
-    };
-  }
+    constructor(view, api, log) {
+        this.view = view;
+        this.api = api;
+        this.log = log;
+
+        this.view.searchInput.addEventListener('keyup', this.debounce(this.loadUsers.bind(this), 500));
+        this.view.loadMoreBtn.addEventListener('click', this.loadMoreUsers.bind(this));
+        this.currentPage = 1;
+        this.usersCount = 0;
+    }
+
+    loadUsers() {
+        this.setCurrentPage(1);
+        this.view.setCounterMessage('');
+        if (this.view.searchInput.value) {
+            this.clearUsers();
+            this.usersRequest(this.view.searchInput.value);
+        } else {
+            this.clearUsers();
+            this.view.toggleLoadMoreBtn(false);
+        }
+    }
+
+    loadMoreUsers() {
+        this.setCurrentPage(this.currentPage + 1);
+        this.usersRequest(this.view.searchInput.value);
+    }
+
+    async usersRequest(searchValue) {
+        let totalCount;
+        let users;
+        let message;
+        try {
+            await this.api.loadUsers(searchValue, this.currentPage).then((res) => {
+                res.json().then(res => {
+                    users = res.items;
+                    totalCount = res.total_count;
+                    message = this.log.counterMessage(res.total_count);
+                    this.setUsersCount(this.usersCount + res.items.length);
+                    this.view.setCounterMessage(message);
+                    this.view.toggleLoadMoreBtn(totalCount > 20 && this.usersCount !== totalCount);
+                    users.forEach(user => this.view.createUser(user));
+                });
+            });
+        } catch (e) {
+            console.log('Error: ' + e);
+        }
+    }
+
+    clearUsers() {
+        this.view.usersList.innerHTML = '';
+    }
+
+    debounce(func, wait, immediate) {
+        let timeout;
+        return function () {
+            const context = this, args = arguments;
+            const later = function () {
+                timeout = null;
+                if (!immediate) func.apply(context, args);
+            };
+            const callNow = immediate && !timeout;
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+            if (callNow) func.apply(context, args);
+        };
+    }
 }
